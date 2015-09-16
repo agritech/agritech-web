262fa6a992f2db48312732ac9063bda6e3dc6b93
Landry <defolandry@yahoo.fr>
2015-09-16 08:26:28 +0100

    Ajout des settings facebook

--- app/config/agritech.php
+++ app/config/agritech.php
@@ -3,5 +3,7 @@ return array(
     'app.campagneAgricole' => 2,//campagne agricole en cours
     'app.sms.gateway' => 'PLAYSMS', //NONE, PLAYSMS, ORANGE 
     'provider.google.id' => '866972914917-da9t2oc2mnr47dbjnd3c2v6lr0bbqbhg.apps.googleusercontent.com',
-    'provider.google.secret' => 'FJgMtWdKE6VifS3OFF485kmV'
+    'provider.google.secret' => 'FJgMtWdKE6VifS3OFF485kmV',
+    'provider.facebook.id' => '471238549715132',
+    'provider.facebook.secret' => 'ded5cf76f99a02099b4342e66bdd6254'
 );
